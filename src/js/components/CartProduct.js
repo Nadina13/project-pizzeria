7e371e777edcd4781d69fda784f9207f9a31c415
Nadina Milewska <milewska.nadina@gmail.com>
2020-09-23 23:49:42 +0200

    Add new files - task 10.1 & 10.2

---
+++ src/js/components/CartProduct.js
@@ -0,0 +1,91 @@
+import { select } from './settings.js';
+import amountWidget from './components/AmountWidget.js';
+
+class CartProduct {
+  constructor(menuProduct, element) {
+    const thisCartProduct = this;
+
+    thisCartProduct.getElements(element);
+    thisCartProduct.initAmountWidget();
+    thisCartProduct.initActions();
+
+    thisCartProduct.id = menuProduct.id;
+    thisCartProduct.name = menuProduct.name;
+    thisCartProduct.priceSingle = menuProduct.priceSingle;
+    thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amountWidget.value;
+    thisCartProduct.amount = thisCartProduct.amountWidget.value;
+    thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+    console.log('new CartProduct', thisCartProduct);
+    console.log('product data', menuProduct);
+
+  }
+
+  getElements(element) {
+    const thisCartProduct = this;
+
+    thisCartProduct.dom = {};
+    thisCartProduct.dom.wrapper = element;
+
+    thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+    thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+    thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+    thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+
+  }
+
+  initAmountWidget() {
+    const thisCartProduct = this;
+
+    thisCartProduct.amountWidget = new amountWidget(thisCartProduct.dom.amountWidget);
+
+    thisCartProduct.dom.amountWidget.addEventListener('updated', function () {
+      thisCartProduct.amount = thisCartProduct.amountWidget.value;
+      thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
+
+      thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+    });
+  }
+
+  remove() {
+    const thisCartProduct = this;
+
+    const event = new CustomEvent('remove', {
+      bubbles: true,
+      detail: {
+        cartProduct: thisCartProduct,
+      },
+    });
+
+    thisCartProduct.dom.wrapper.dispatchEvent(event);
+  }
+
+  initActions() {
+    const thisCartProduct = this;
+
+    thisCartProduct.dom.edit.addEventListener('click', function (event) {
+      event.preventDefault();
+    });
+
+    thisCartProduct.dom.remove.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisCartProduct.remove();
+      console.log(thisCartProduct);
+    });
+  }
+
+  getData() {
+    const thisCartProduct = this;
+
+    const productInfo = {
+      id: thisCartProduct.id,
+      price: thisCartProduct.price,
+      priceSingle: thisCartProduct.priceSingle,
+      amount: thisCartProduct.amount,
+      params: thisCartProduct.params,
+    };
+    return productInfo;
+  }
+}
+
+export default CartProduct;
